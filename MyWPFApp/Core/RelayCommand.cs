fe395f504de55015bba200af49ae050f79513b5b
DESKTOP-QPQLO09\locald <mohib@esparkconsultants.com>
2023-06-21 21:08:27 +0500

    first commit

---
+++ MyWPFApp/Core/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyWPFApp.Core
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object,bool> _executeFunc;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public RelayCommand(Action<object> execute, Func<object,bool> executeFunc = null)
+        {
+            _execute = execute;
+            _executeFunc = executeFunc;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return _executeFunc == null || _executeFunc(parameter) ;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
